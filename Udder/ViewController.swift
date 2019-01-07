be3db4515adced35f141f5caeafcd2bd1bba14e4
Ying Zhou <ying.zhou@qc.cuny.edu>
2019-01-07 08:38:00 -0500

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Ying Zhou on 1/7/19.
+//  Copyright © 2019 3cslab.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
